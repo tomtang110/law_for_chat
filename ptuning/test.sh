1db17feb9abf598161ba5c1d0e03a475768c1de6
tomtang110 <tomtang110@outlook.com>
2023-04-22 21:53:20 +0800

    merge zecheng dev

--- ptuning/test.sh
+++ ptuning/test.sh
@@ -6,4 +6,4 @@ CUDA_VISIBLE_DEVICES=0 python3 demo.py \
      --model_name_or_path THUDM/chatglm-6b \
      --ptuning_checkpoint ./output/$CHECKPOINT/checkpoint-$STEP \
      --cache_dir ./cache_dir  \
-     --pre_seq_len $PRE_SEQ_LEN \
+     --pre_seq_len $PRE_SEQ_LEN
