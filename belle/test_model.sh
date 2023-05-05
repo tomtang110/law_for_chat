088892801203979676302c5cb6e2568c9759ed44
tomtang110 <tomtang110@outlook.com>
2023-05-05 23:33:30 +0800

    560m

--- belle/test_model.sh
+++ belle/test_model.sh
@@ -1,4 +1,4 @@
 python test_model.py \
-      --model_name_or_path bigscience/bloomz-560 \
+      --model_name_or_path bigscience/bloomz-560m \
       --cache_dir ./cache_dir \
       --finetuned_model_name_or_path ./output \
